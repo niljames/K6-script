0a629b0ea03a3a145d95bd9a3ee3d1dc2766afaa
niljames <njames@dovercorp.com>
2023-05-25 11:33:40 +0530

    Commit #1.

--- K6-Samplegit.js
+++ K6-Samplegit.js
@@ -1,8 +1,8 @@
 import http from 'k6/http';
-import * as add from 'commonmethod/common_method.js';
+import { addition } from './commonmethod/common_method.js';
 
 export default function () {
     http.get('https://test.k6.io');
-    const s = add.addition(2, 3);
+    const s = addition(2, 3);
     console.log("Sum = " + s)
 }
