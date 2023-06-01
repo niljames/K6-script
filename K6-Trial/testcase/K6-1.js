62fe0d80c47a0e4dbc651b0c6ca4524cab427e49
niljames <njames@dovercorp.com>
2023-06-02 00:22:54 +0530

    Last Commit

--- K6-Trial/testcase/K6-1.js
+++ K6-Trial/testcase/K6-1.js
@@ -3,7 +3,6 @@ import * as add_div from '../commonmethod/common_method.js';
 import * as sub_mul from '../cm/cm2/cm.js';
 
 export default function () {
-    http.get('https://test.k6.io');
     const s = add_div.addition(6, 3);
     console.log("Sum = " + s)
     const q = add_div.division(6, 3);
