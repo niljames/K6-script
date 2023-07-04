c0e3ac75f0c9e2afcd8b20bf0facfe325cbbb98f
niljames <njames@dovercorp.com>
2023-07-04 09:37:02 +0530

    Added Tags.

--- K6-Trial/testcase/K6-1.js
+++ K6-Trial/testcase/K6-1.js
@@ -2,8 +2,16 @@ import http from 'k6/http';
 import * as add_div from '../commonmethod/common_method.js';
 import * as sub_mul from '../cm/cm2/cm.js';
 
+
+export const options = {
+    tags: {
+        API_Type: __ENV.Type,
+        TimeStamp: String(Date.now())
+    }
+};
+
 export default function () {
-    http.get('https://test.k6.io');
+    const res = http.get('https://test.k6.io');
     const s = add_div.addition(6, 3);
     console.log("Sum = " + s)
     const q = add_div.division(6, 3);
